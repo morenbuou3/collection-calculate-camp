846a74a3cadc6a31f941cb3063b78b5ee02c7bae
Jiahao Wang <jhwang@CNJiahao.local>
2018-09-04 17:48:54 +0800

    [Jiahao] finish flaten

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function double_to_one(collection) {
-
   //在这里写入代码
+  return Array.from(new Set(collection.reduce((acc, val) => Array.isArray(val) ? acc.concat(double_to_one(val)) : acc.concat(val), [])));
 }
 
 module.exports = double_to_one;

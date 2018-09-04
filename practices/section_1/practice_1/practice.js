2aa2d54cba89452d44642894de0f3c6a269fb9fa
Jiahao Wang <jhwang@CNJiahao.local>
2018-09-04 21:28:46 +0800

    [Jiahao] finish section_1

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,5 +1,6 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  return collection_a.filter(n => new Set(collection_b).has(n));
 }
 
 module.exports = collect_same_elements;
--- practices/section_1/practice_3/practice.js
+++ practices/section_1/practice_3/practice.js
@@ -1,5 +1,6 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  return collection_a.filter(n => new Set(object_b.value).has(n));
 }
 
 module.exports = collect_same_elements;

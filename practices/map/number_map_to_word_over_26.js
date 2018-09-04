60377594b1f0dd050e7116db64bddcde9847490a
Jiahao Wang <jhwang@CNJiahao.local>
2018-09-04 17:59:19 +0800

    [Jiahao] finish map

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,20 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  return collection.map(n => convert(n));
 };
 
+
+function convert(num){
+  var result = [];
+  while(num){
+    var t = num % 26;
+    if(!t){
+      t = 26;
+      -- num;
+    }
+    result.push(String.fromCodePoint(t + 96));
+    num = ~~(num / 26);
+  }
+  return result.reverse().join('');
+}
 module.exports = number_map_to_word_over_26;

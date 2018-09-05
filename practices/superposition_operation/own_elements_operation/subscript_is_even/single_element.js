f0518a0703f6c64fd4df982a7ca556ca2a828a06
Jiahao Wang <jhwang@CNJiahao.local>
2018-09-05 09:16:20 +0800

    [Jiahao] finish single

--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,6 +1,6 @@
 'use strict';
 var single_element = function(collection){
-  let numberWithEvenIndex = collection.filter((num, index) => index % 2 === 1);
+  let numberWithEvenIndex = collection.filter((num, index) => index % 2 !== 0);
   return numberWithEvenIndex.filter(num => numberWithEvenIndex.indexOf(num) === numberWithEvenIndex.lastIndexOf(num));
 };
 module.exports = single_element;
